9aa7276e7df80115e5826e5d4ed6d54bd396ec4f
Long <15257546+lzztt@users.noreply.github.com>
2016-10-07 22:38:47 -0500

    add solutions

--- CtCI-6th-Edition-cpp/04_Trees_and_Graphs/08_First_Common_Ancestor/firstCommonAncestor.cpp
+++ CtCI-6th-Edition-cpp/04_Trees_and_Graphs/08_First_Common_Ancestor/firstCommonAncestor.cpp
@@ -1,5 +1,47 @@
+#include <vector>
 #include "firstCommonAncestor.hpp"
 
-p_node_t firstCommonAncestor(const p_node_t& root, const p_node_t& n1, const p_node_t& n2) {
+p_node_t traverse(const p_node_t& root, const p_node_t& node, std::vector<int>& move, int depth) {
+  if (root == nullptr) return nullptr;
+
+  if (move.size() == depth) move.push_back(0);
+  if (root == node) return node;
+
+  auto p = traverse(root->left, node, move, depth+1);
+  if (p == node) {
+    move[depth] = -1;
+    return node;
+  }
+
+  p = traverse(root->right, node, move, depth+1);
+  if (p == node) {
+    move[depth] = 1;
+    return node;
+  }
+
   return nullptr;
+}
+
+p_node_t firstCommonAncestor(const p_node_t& root, const p_node_t& n1, const p_node_t& n2) {
+  if (root == nullptr || n1 == nullptr || n2 == nullptr) return nullptr;
+  if (n1 == n2) return n1;
+
+  std::vector<int> move1;
+  auto p = traverse(root, n1, move1, 0);
+  if (p == nullptr) return nullptr;
+  if (move1.empty()) return root;
+
+  std::vector<int> move2;
+  p = traverse(root, n2, move2, 0);
+  if (p == nullptr) return nullptr;
+  if (move2.empty()) return root;
+
+  p = root;
+  for (auto i1 = move1.begin(), i2 = move2.begin(); *i1 == *i2; ++i1, ++i2) {
+    if (*i1 == 0 || *i2 == 0) return p;
+
+    p = (*i1 == -1 ? p->left : p->right);
+  }
+
+  return p;
 }
--- CtCI-6th-Edition-cpp/04_Trees_and_Graphs/08_First_Common_Ancestor/firstCommonAncestor.test.cpp
+++ CtCI-6th-Edition-cpp/04_Trees_and_Graphs/08_First_Common_Ancestor/firstCommonAncestor.test.cpp
@@ -7,4 +7,23 @@ TEST_CASE("firstCommonAncestor test", "[firstCommonAncestor]") {
   auto root = BT::create("[]");
   auto n1 = root, n2 = root;
   REQUIRE(firstCommonAncestor(root, n1, n2) == nullptr);
+
+  root = BT::create("[1,2,3,4,null,null,5,null,null,6,7,null,null,8,null,null,9,10,null,11]");
+  n1 = root->left->left->left;
+  n2 = root->left->left->right;
+  REQUIRE(n1->data == 4);
+  REQUIRE(n2->data == 5);
+  REQUIRE(firstCommonAncestor(root, n1, n2) == root->left->left);
+
+  n2 = root->left->right->left;
+  REQUIRE(n2->data == 7);
+  REQUIRE(firstCommonAncestor(root, n1, n2) == root->left);
+
+  n2 = root->right->left->right;
+  REQUIRE(n2->data == 11);
+  REQUIRE(firstCommonAncestor(root, n1, n2) == root);
+
+  n2 = root->left->left;
+  REQUIRE(n2->data == 3);
+  REQUIRE(firstCommonAncestor(root, n1, n2) == root->left->left);
 }
