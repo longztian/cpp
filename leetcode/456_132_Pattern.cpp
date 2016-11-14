6104d28714484c1b861d0a91203305b4d1b210bb
Long <15257546+lzztt@users.noreply.github.com>
2016-11-14 13:36:43 -0600

    Update 456_132_Pattern.cpp

--- leetcode/456_132_Pattern.cpp
+++ leetcode/456_132_Pattern.cpp
@@ -1,38 +1,31 @@
-bool myComp(const pair<const int, int>& i, const pair<const int, int>& j) {
-    return i.first < j.first;
-}
-
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         if (nums.size() < 3) return false;
 
-        vector<int> min_i(nums.size(), INT_MAX);
-        int min = INT_MAX;
+        vector<int> mins(nums.size(), INT_MAX);
+        int minv = INT_MAX;
         for (int i = 1; i < nums.size(); ++i) {
-            if (min > nums[i - 1]) min = nums[i - 1];
-            if (min < nums[i]) min_i[i] = min;
+            if (minv > nums[i - 1]) minv = nums[i - 1];
+            if (minv < nums[i] - 1) mins[i] = minv;
         }
 
-        map<int, int> valueLastIndex;
+        map<int, int> valueLast;
         for (int i = 0; i < nums.size(); ++i) {
-            valueLastIndex[nums[i]] = i;
+            valueLast[nums[i]] = i;
         }
 
-        int i, j, k, max_k;
-        for (j = 1; j < nums.size() - 1; ++j) {
-            if (min_i[j] == INT_MAX) continue;
-
-            auto itb = upper_bound(valueLastIndex.begin(), valueLastIndex.end(), pair<const int, int>({min_i[j], 0}), myComp);
-            auto ite = lower_bound(valueLastIndex.begin(), valueLastIndex.end(), pair<const int, int>({nums[j], 0}), myComp);
-
-            while (itb != ite) {
-                if (itb->second > j) return true;
-                ++itb;
+        for (int j = 1; j < nums.size() - 1; ++j) {
+            if (mins[j] != INT_MAX) {
+                auto itb = ++(valueLast.find(mins[j]));
+                auto ite = valueLast.find(nums[j]);
+                while (itb != ite) {
+                    if ((itb)->second > j) return true;
+                    ++itb;
+                }
             }
         }
 
         return false;
     }
-
 };
