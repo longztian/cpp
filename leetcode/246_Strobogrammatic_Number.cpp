7404840421f1ddde521ab08e14e4d4cba78ea59b
Long <15257546+lzztt@users.noreply.github.com>
2016-11-18 21:51:26 -0600

    update solution

---
+++ leetcode/246_Strobogrammatic_Number.cpp
@@ -0,0 +1,21 @@
+unordered_map<char, char> digits({
+  {'0', '0'},
+  {'1', '1'},
+  {'6', '9'},
+  {'8', '8'},
+  {'9', '6'}
+});
+
+class Solution {
+public:
+  bool isStrobogrammatic(string num) {
+    unordered_map<char, char>::iterator it;
+
+    for (auto i = num.data(), j = i + num.size() - 1; i <= j; ++i, --j) {
+      it = digits.find(*i);
+      if (it == digits.end() || it->second != *j) return false;
+    }
+
+    return true;
+  }
+};
---
+++ leetcode/247_Strobogrammatic_Number_II.cpp
@@ -0,0 +1,47 @@
+vector<char> first     ({'1', '6', '8', '9'});
+vector<char> firstMatch({'1', '9', '8', '6'});
+
+vector<char> digit     ({'0', '1', '6', '8', '9'});
+vector<char> digitMatch({'0', '1', '9', '8', '6'});
+
+vector<char> single    ({'0', '1', '8'});
+
+class Solution {
+public:
+  vector<string> findStrobogrammatic(int n) {
+    if (n <= 0) return vector<string>();
+
+    vector<string> results;
+    string num(n, '0');
+
+    myGenerateNumber(0, n % 2, num, results);
+
+    return results;
+  }
+
+private:
+  void myGenerateNumber(int d, bool oddDigit, string& num, vector<string>& results) {
+    if (d == num.size() / 2) {
+      if (oddDigit) {
+        for (int i = 0; i < single.size(); ++i) {
+          num[d] = single[i];
+          myGenerateNumber(d, false, num, results);
+        }
+      } else {
+        results.push_back(num);
+      }
+    } else if (d == 0) {
+      for (int i = 0; i < first.size(); ++i) {
+        num[d] = first[i];
+        num[num.size() - d - 1] = firstMatch[i];
+        myGenerateNumber(d + 1, oddDigit, num, results);
+      }
+    } else {
+      for (int i = 0; i < digit.size(); ++i) {
+        num[d] = digit[i];
+        num[num.size() - d - 1] = digitMatch[i];
+        myGenerateNumber(d + 1, oddDigit, num, results);
+      }
+    }
+  }
+};
