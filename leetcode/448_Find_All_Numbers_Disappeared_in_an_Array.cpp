231e60435896cbd4314bba1da5dd57d1f9e03cc9
Long <15257546+lzztt@users.noreply.github.com>
2016-11-20 21:17:20 -0600

    update solution

---
+++ leetcode/448_Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -0,0 +1,46 @@
+// O(n) space
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        const int n = nums.size();
+        if (n < 2) return vector<int>();
+
+        vector<int> count(n + 1, 0);
+        for (int i : nums) ++count[i];
+
+        vector<int> miss;
+        for (int i = 1; i <= n; ++i) {
+            if (count[i] == 0) miss.push_back(i);
+        }
+
+        return miss;
+    }
+};
+
+// O(1) space
+// mark number by *= -1, so can change the values back
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        const int n = nums.size();
+        if (n < 2) return vector<int>();
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                int j = nums[i] - 1, tmp;
+                while (nums[j] != 0) {
+                    tmp = nums[j] - 1;
+                    nums[j] *= -1;
+                    j = tmp;
+                }
+            }
+        }
+
+        vector<int> miss;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) miss.push_back(i + 1);
+        }
+
+        return miss;
+    }
+};
