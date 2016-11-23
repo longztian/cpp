a746d3f48291a183e1cd17849f8989ef33f51772
Long <15257546+lzztt@users.noreply.github.com>
2016-11-23 16:56:54 -0600

    update solution

--- leetcode/375_Guess_Number_Higher_or_Lower_II.cpp
+++ leetcode/375_Guess_Number_Higher_or_Lower_II.cpp
@@ -20,3 +20,30 @@ private:
         return minMax;
     }
 };
+
+// miniMax
+// state: int (begin, end);
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+        vector<vector<int>> cache(n + 1, vector<int>(n + 1, 0));
+        return myMiniMax(1, n, cache);
+    }
+
+private:
+    int myMiniMax(int begin, int end, vector<vector<int>>& cache) {
+        if (begin >= end) return 0;
+        if (begin + 1 == end) return begin;
+
+        if (cache[begin][end] != 0) return cache[begin][end];
+
+        int maxMoney, minMaxMoney = INT_MAX;
+        for (int i = begin + 1; i < end; ++i) {
+            maxMoney = i + max(myMiniMax(begin, i - 1, cache), myMiniMax(i + 1, end, cache));
+            if (minMaxMoney > maxMoney) minMaxMoney = maxMoney;
+        }
+
+        cache[begin][end] = minMaxMoney;
+        return minMaxMoney;
+    }
+};
