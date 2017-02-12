fd6e5aec411ee3248aec3aa16bcb4d047df9988b
Long <15257546+lzztt@users.noreply.github.com>
2017-02-11 18:50:13 -0600

    update solutions

---
+++ leetcode/387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,30 @@
+const int NLETTERS = 26;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        const int n = s.size();
+        const int NOTFOUND = n;
+        const int REPEATED = n + 1;
+
+        int indexes[NLETTERS];
+        std::fill_n(indexes, NLETTERS, NOTFOUND);
+
+        int id = 0;
+        for (int i = 0; i < n; ++i) {
+            id = s[i] - 'a';
+
+            if (indexes[id] == REPEATED) continue;
+
+            if (indexes[id] == NOTFOUND) {
+                indexes[id] = i;  // first time seen, save index
+            } else {
+                indexes[id] = REPEATED;  // mark as repeated
+            }
+        }
+
+        int minId = *std::min_element(indexes, indexes + NLETTERS);
+
+        return minId < n ? minId : -1;
+    }
+};
